113a86ab5ba9f52ae7e72a24a95d72a256c6b0d0
ESProshina <ESRproshina@beeline.ru>
2026-06-02 20:36:21 +0300

    Add junit examples

--- src/test/java/tests/exampies/JUnitExamplesTests.java
+++ src/test/java/tests/exampies/JUnitExamplesTests.java
@@ -1,14 +1,35 @@
 package tests.exampies;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 public class JUnitExamplesTests {
+
+
+    @BeforeAll
+    static void startup(){
+        System.out.println("tests startub");
+    }
+    @AfterAll
+    static void teardown(){
+        System.out.println("tests ended");
+    }
+
+    @BeforeEach
+    void startBrowser(){
+                System.out.println("-browser has started");
+    }
+    @AfterEach
+    void stopBrowser(){
+        System.out.println("-browser has stopped");
+    }
+
     @Test
     void firstTest() {
-        System.out.println("firstTest");
+        System.out.println("--firstTest");
     }
     @Test
     void secondTest() {
-        System.out.println("secondTest");
+        System.out.println("--secondTest");
 
     }
 }
